3094dfd47ce863a83a08e5e4555f229d11717bb2
Hellman Studios <jesse.hellman@hellmanstudios.fi>
2024-09-29 11:21:32 +0300

    initialize spring boot project

---
+++ src/main/java/com/hellmanstudios/rentanything/RentanythingApplication.java
@@ -0,0 +1,13 @@
+package com.hellmanstudios.rentanything;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentanythingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentanythingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hellmanstudios/rentanything/RentanythingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hellmanstudios.rentanything;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentanythingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
