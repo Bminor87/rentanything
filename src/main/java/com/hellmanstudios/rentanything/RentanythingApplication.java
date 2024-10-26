b46d627816fe10e5c452e619b3efbfa9a4fccb84
Hellman Studios <jesse.hellman@hellmanstudios.fi>
2024-10-26 08:49:17 +0300

    remove demo content because database is now permanent

--- src/main/java/com/hellmanstudios/rentanything/RentanythingApplication.java
+++ src/main/java/com/hellmanstudios/rentanything/RentanythingApplication.java
@@ -28,38 +28,38 @@ public class RentanythingApplication {
 		SpringApplication.run(RentanythingApplication.class, args);
 	}
 
-	@Bean
-	public CommandLineRunner demo(CategoryRepository categoryRepository, ItemRepository itemRepository, UserRepository userRepository, RentalRepository rentalRepository, RoleRepository roleRepository) {
-		return (args) -> {
+	// @Bean
+	// public CommandLineRunner demo(CategoryRepository categoryRepository, ItemRepository itemRepository, UserRepository userRepository, RentalRepository rentalRepository, RoleRepository roleRepository) {
+	// 	return (args) -> {
 			 
-			Role userRole = roleRepository.save(new Role("USER"));
-			Role adminRole = roleRepository.save(new Role("ADMIN"));
+	// 		Role userRole = roleRepository.save(new Role("USER"));
+	// 		Role adminRole = roleRepository.save(new Role("ADMIN"));
 
-			User user = userRepository.save(new User("user", "$2y$10$JWF.ho1LMyJx/6iUajlmB.unoynCy7IkS91upvZwXnpJ7YcnOG.1C", "user@rentanything.com", userRole));
-			User admin = userRepository.save(new User("admin", "$2y$10$PkY2eBIXqNpmF1vgdQv3q.gd1rmtDHnOw31Gf3USUncZk3XcIVOjW", "admin@rentanything.com", adminRole));
+	// 		User user = userRepository.save(new User("user", "$2y$10$JWF.ho1LMyJx/6iUajlmB.unoynCy7IkS91upvZwXnpJ7YcnOG.1C", "user@rentanything.com", userRole));
+	// 		User admin = userRepository.save(new User("admin", "$2y$10$PkY2eBIXqNpmF1vgdQv3q.gd1rmtDHnOw31Gf3USUncZk3XcIVOjW", "admin@rentanything.com", adminRole));
 
-			Category category1 = categoryRepository.save(new Category("Musical Instruments", "Instruments for gigs or a day in the studio"));
-			Category category2 = categoryRepository.save(new Category("Tools", "Tools for a project or a quick fix"));
-			Category category3 = categoryRepository.save(new Category("Costumes", "Costumes for a party or a play"));
-			Category category4 = categoryRepository.save(new Category("Electronic Devices", "Devices for a presentation or a movie night"));
-			Category category5 = categoryRepository.save(new Category("Games", "Games for a game night or a rainy day"));
+	// 		Category category1 = categoryRepository.save(new Category("Musical Instruments", "Instruments for gigs or a day in the studio"));
+	// 		Category category2 = categoryRepository.save(new Category("Tools", "Tools for a project or a quick fix"));
+	// 		Category category3 = categoryRepository.save(new Category("Costumes", "Costumes for a party or a play"));
+	// 		Category category4 = categoryRepository.save(new Category("Electronic Devices", "Devices for a presentation or a movie night"));
+	// 		Category category5 = categoryRepository.save(new Category("Games", "Games for a game night or a rainy day"));
 
-			Item guitar = itemRepository.save(new Item("Guitar", "A beautiful guitar", "image1.jpg", 10.0, category1));
-			itemRepository.save(new Item("Stradivari Violin", "A beautiful violin", "image2.jpg", 100.0, category1));
-			itemRepository.save(new Item("Steinway Piano", "A beautiful piano", "image3.jpg", 50.0, category1));
-			itemRepository.save(new Item("Drill", "A powerful drill", "image4.jpg", 5.0, category2));
-			itemRepository.save(new Item("Saw", "A powerful saw", "image5.jpg", 3.0, category2));
-			Item hammer = itemRepository.save(new Item("Hammer", "A powerful hammer", "image6.jpg", 2.0, category2));
-			itemRepository.save(new Item("Spiderman Costume", "Full Spiderman costume", "image7.jpg", 20.0, category3));
-			itemRepository.save(new Item("Batman Costume", "Full Batman costume", "image8.jpg", 20.0, category3));
-			itemRepository.save(new Item("Princess' Dress", "Wanna play snow white?", "image9.jpg", 20.0, category3));
-			itemRepository.save(new Item("Projector", "A Projector", "image10.jpg", 15.0, category4));
-			itemRepository.save(new Item("Camera", "A Camera", "image11.jpg", 10.0, category4));
-			Item chess = itemRepository.save(new Item("Chess", "A maple wood chess set", "image12.jpg", 2.0, category5));
-			itemRepository.save(new Item("Monopoly", "A monopoly game", "image13.jpg", 2.0, category5));
-			itemRepository.save(new Item("Playing Cards", "Playing Cards for poker etc.", "image14.jpg", 2.0, category5));
+	// 		Item guitar = itemRepository.save(new Item("Guitar", "A beautiful guitar", "image1.jpg", 10.0, category1));
+	// 		itemRepository.save(new Item("Stradivari Violin", "A beautiful violin", "image2.jpg", 100.0, category1));
+	// 		itemRepository.save(new Item("Steinway Piano", "A beautiful piano", "image3.jpg", 50.0, category1));
+	// 		itemRepository.save(new Item("Drill", "A powerful drill", "image4.jpg", 5.0, category2));
+	// 		itemRepository.save(new Item("Saw", "A powerful saw", "image5.jpg", 3.0, category2));
+	// 		Item hammer = itemRepository.save(new Item("Hammer", "A powerful hammer", "image6.jpg", 2.0, category2));
+	// 		itemRepository.save(new Item("Spiderman Costume", "Full Spiderman costume", "image7.jpg", 20.0, category3));
+	// 		itemRepository.save(new Item("Batman Costume", "Full Batman costume", "image8.jpg", 20.0, category3));
+	// 		itemRepository.save(new Item("Princess' Dress", "Wanna play snow white?", "image9.jpg", 20.0, category3));
+	// 		itemRepository.save(new Item("Projector", "A Projector", "image10.jpg", 15.0, category4));
+	// 		itemRepository.save(new Item("Camera", "A Camera", "image11.jpg", 10.0, category4));
+	// 		Item chess = itemRepository.save(new Item("Chess", "A maple wood chess set", "image12.jpg", 2.0, category5));
+	// 		itemRepository.save(new Item("Monopoly", "A monopoly game", "image13.jpg", 2.0, category5));
+	// 		itemRepository.save(new Item("Playing Cards", "Playing Cards for poker etc.", "image14.jpg", 2.0, category5));
 
-		};
-	}
+	// 	};
+	// }
 
 }
